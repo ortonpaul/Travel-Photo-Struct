d69a08c0b27685cf5ae1312f64de637393fbfeaf
Paul Orton <ortonpaul18@gmail.com>
2019-02-20 17:38:40 -0600

    Created struct and the instance photo1

--- main.swift
+++ main.swift
@@ -1,12 +1,23 @@
-//
-//  main.swift
-//  Travel Photo Struct
-//
-//  Created by Paul Orton on 2/20/19.
-//  Copyright © 2019 Paul Orton. All rights reserved.
-//
+// Travel Photo Struct
+// Created by Paul Orton
+// for INFOTC 4405 SP19
 
 import Foundation
 
-print("Hello, World!")
+struct TravelPhoto {
+    let latitude: Double
+    let longitude: Double
+    let title: String
+    let description: String
+    let image: String
+    let urlString: String
+}
 
+let photo1 = TravelPhoto(latitude: 38.94733, longitude: -92.328466, title: "The Mizzou Columns", description: "University of Missour six iconic columns", image: "columns.png", urlString: "http://missouri.edu/about/history/columns.php")
+
+print(photo1.latitude)
+print(photo1.longitude)
+print(photo1.title)
+print(photo1.description)
+print(photo1.image)
+print(photo1.urlString)
