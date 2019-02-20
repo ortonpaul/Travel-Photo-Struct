615d6b4045e6ddb699c5d73df349f8c7371451c1
Paul Orton <ortonpaul18@gmail.com>
2019-02-20 17:29:12 -0600

    First commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Travel Photo Struct
+//
+//  Created by Paul Orton on 2/20/19.
+//  Copyright © 2019 Paul Orton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
